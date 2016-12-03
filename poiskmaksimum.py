70118a71778192c89f5ea18bc2439981f7ad4763
pussydestroyer <minenotch@mail.ru>
2016-12-03 23:04:35 +0300

    2 i 3

---
+++ poiskmaksimum.py
@@ -0,0 +1,16 @@
+#Задание списка
+A = list(map(int, input().split()))
+# Нахождение максимума
+n = 0
+s = 0
+for i in A:
+    if i == 0:
+        break
+    if i > s:
+        s = i
+for j in A:
+    if j == 0:
+        break
+    if j == s:
+        n += 1
+print(n)
